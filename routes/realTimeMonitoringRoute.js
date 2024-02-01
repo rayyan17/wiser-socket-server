5a524f30d2664c2aeee1444e19ebadf88b7a528e
Saadi07 <saadiawan09@gmail.com>
2024-02-01 18:16:46 +0500

    added real time ambient and thermister data

--- routes/realTimeMonitoringRoute.js
+++ routes/realTimeMonitoringRoute.js
@@ -1,6 +1,8 @@
 import {
   fetchRealTimeCurrentData,
   fetchMostRecentDataPoint,
+  fetchRealTimeAmbientData,
+  fetchRealTimeThermisterData
 } from "../services/realTimeMonitoringService.js";
 
 export default function realTimeMonitoringRoute(fastify, options, done) {
@@ -37,7 +39,7 @@ export default function realTimeMonitoringRoute(fastify, options, done) {
                   status: "warning",
                   average_current: 0.0,
                   total_current: 0.0,
-                  timestamp: currentTime
+                  timestamp: currentTime,
                 })
               );
             }
@@ -55,5 +57,48 @@ export default function realTimeMonitoringRoute(fastify, options, done) {
       });
     }
   );
+
+  fastify.get(
+    "/ambient-monitoring/:macAddress",
+    { websocket: true },
+    (connection /* SocketStream */, req /* FastifyRequest */) => {
+      const { db } = options;
+      const { macAddress } = req.params;
+
+      const interval = setInterval(async () => {
+        try {
+          const data = await fetchRealTimeAmbientData(db, macAddress);
+          connection.socket.send(JSON.stringify(data));
+        } catch (error) {
+          console.error("Error fetching data from MongoDB:", error);
+        }
+      }, 5000);
+
+      connection.socket.on("close", () => {
+        clearInterval(interval);
+      });
+    }
+  );
+  fastify.get(
+    "/thermister-monitoring/:macAddress",
+    { websocket: true },
+    (connection /* SocketStream */, req /* FastifyRequest */) => {
+      const { db } = options;
+      const { macAddress } = req.params;
+  
+      const interval = setInterval(async () => {
+        try {
+          const data = await fetchRealTimeThermisterData(db, macAddress);
+          connection.socket.send(JSON.stringify(data));
+        } catch (error) {
+          console.error("Error fetching data from MongoDB:", error);
+        }
+      }, 5000);
+  
+      connection.socket.on("close", () => {
+        clearInterval(interval);
+      });
+    }
+  );  
   done();
 }
--- services/realTimeMonitoringService.js
+++ services/realTimeMonitoringService.js
@@ -68,3 +68,89 @@ export async function fetchMostRecentDataPoint(db, macAddress) {
     throw error;
   }
 }
+
+export async function fetchRealTimeAmbientData(db, macAddress) {
+  const currentTime = new Date();
+  const startTime = new Date(currentTime.getTime() - 100 * 1000); // 15 seconds ago
+  try {
+    const collectionAmbient = db.collection('ambients');
+
+    const result = await collectionAmbient
+      .aggregate([
+        {
+          $match: { mac: macAddress, created_at: { $gte: startTime, $lte: currentTime } }
+        },
+        {
+          $project: {
+            _id: 0,
+            amb_temp: 1,
+            ambient_humidity: 1,
+            created_at: {
+              $dateToString: {
+                format: "%Y-%m-%dT%H:%M:%S.%LZ",
+                date: "$created_at",
+              },
+            },
+          },
+        },
+      ])
+      .sort({ created_at: -1 })
+      .limit(1)
+      .toArray();
+
+    if (result.length === 0) {
+      return {
+        results: "No ambient data available of this Node.",
+        status: "error",
+      };
+    }
+
+    return result[0];
+  } catch (error) {
+    console.error("Error fetching ambient data from MongoDB:", error);
+    throw error;
+  }
+}
+
+
+export async function fetchRealTimeThermisterData(db, macAddress) {
+  const currentTime = new Date();
+  const startTime = new Date(currentTime.getTime() - 100 * 1000); // 15 seconds ago
+  try {
+    const collectionAmbient = db.collection('thermisters');
+
+    const result = await collectionAmbient
+      .aggregate([
+        {
+          $match: { mac: macAddress, created_at: { $gte: startTime, $lte: currentTime } }
+        },
+        {
+          $project: {
+            _id: 0,
+            therm_temp: 1,
+            created_at: {
+              $dateToString: {
+                format: "%Y-%m-%dT%H:%M:%S.%LZ",
+                date: "$created_at",
+              },
+            },
+          },
+        },
+      ])
+      .sort({ created_at: -1 })
+      .limit(1)
+      .toArray();
+
+    if (result.length === 0) {
+      return {
+        results: "No thermister data available of this Node.",
+        status: "error",
+      };
+    }
+
+    return result[0];
+  } catch (error) {
+    console.error("Error fetching ambient data from MongoDB:", error);
+    throw error;
+  }
+}
